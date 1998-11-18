a54f6c01abb3fb2f5afdd2f59f8e1a71e7d81944
rk <rk>
1998-11-18 16:00:43 +0000

    Fixed birdy, so tinvoke.x will work properly

--- tests/birdy.c
+++ tests/birdy.c
@@ -2,14 +2,24 @@
 
 #define MAXBUF	1024
 
-int main() {
+int main(int argc, char *argv[]) {
 	char buffer[MAXBUF];
+	char *prgname = *argv++;
+	FILE *in;
 
-	(void) setvbuf(stdout, (char *) NULL, _IOLBF, 0);
-	while (fgets(buffer, MAXBUF, stdin) != (char *) NULL) {
-		if (buffer[strlen(buffer)-1] == '\n')
-			buffer[strlen(buffer)-1] = '\0';
-		(void) fprintf(stdout,"BOL: %s :EOL\n", buffer);
+	while (*argv != (char *) NULL) {
+		if ((in = fopen(*argv, "r")) == (FILE *) NULL) {
+			(void) fprintf(stderr,"%s: can't open '%s'\n",
+				prgname, *argv);
+		}
+		(void) setvbuf(stdout, (char *) NULL, _IOLBF, 0);
+		while (fgets(buffer, MAXBUF, in) != (char *) NULL) {
+			if (buffer[strlen(buffer)-1] == '\n')
+				buffer[strlen(buffer)-1] = '\0';
+			(void) fprintf(stdout,"BOL: %s :EOL\n", buffer);
+		}
+		(void) fclose(in);
+		argv++;
 	}
 	return 0;
 }
