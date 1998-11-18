73febf38200a59995458a23bf0c3a25dec50265e
rk <rk>
1998-11-18 15:34:40 +0000

    removed USMID

--- strfns/macrosub.c
+++ strfns/macrosub.c
@@ -1,5 +1,4 @@
-static char USMID[]="%W%";
-static const char RCSID[]="@(#)$Id: macrosub.c,v 1.3 1998/11/07 00:20:57 rk Exp $";
+static const char RCSID[]="@(#)$Id: macrosub.c,v 1.4 1998/11/18 15:34:40 rk Exp $";
 static char AUTHOR[]="@(#)macrosub.c 1.0 08/30/94 R.K.Owen,PhD";
 
 /* macstrncpy(string1,string2,n) - Macro string copy
